48475c40d90baf0d0ea19b49356a5ff331cb64b8
armandbissesar <armandbissesar@gmail.com>
2019-01-28 22:20:23 +0100

    init

--- js/main.js
+++ js/main.js
@@ -1,18 +1,72 @@
-var bladwijzer = document.querySelectorAll("section article button")
-
-
-for (var i = 0; i < bladwijzer.length; i++) {
-    bladwijzer[i].addEventListener('click', bladwijzerFunctie)
-}
+var bladwijzer = document.querySelectorAll("button.bladwijzer");
+var duimfout = document.querySelectorAll("button.fout");
+var duimgoed = document.querySelectorAll("button.goed");
+var verwardeImages = document.querySelectorAll(".de-verwarde-man img");
 
 function bladwijzerFunctie(e) {
-    if (e.target.classList.contains('active')) {
-        e.target.classList.remove('active')
+    var img = e.target.childNodes[0]
+    if (img.classList.contains('active')) {
+        img.classList.remove('active');
+        img.src = "images/symbolen/unfill.png";
 
     } else {
-        e.target.classList.add('active')
+        img.classList.add('active');
+        img.src = "images/symbolen/filled.png";
+
 
     }
-    console.log(e.target);
+    console.log(img);
 
+}
+
+function duimgoedFunctie(e) {
+    var img = e.target.childNodes[0];
+    if (img.classList.contains('active')) {
+        img.classList.remove('active');
+        img.src = "images/symbolen/duimgoed.svg";
+
+    } else {
+        img.classList.add('active');
+        img.src = "images/symbolen/duimgoedfilled.svg";
+    }
+    console.log(img);
+}
+
+function duimfoutFunctie(e) {
+    var img = e.target.childNodes[0];
+    if (img.classList.contains('active')) {
+        img.classList.remove('active');
+        img.src = "images/symbolen/duimfout.svg";
+
+    } else {
+        img.classList.add('active');
+        img.src = "images/symbolen/duimfoutFilled.svg";
+    }
+    console.log(img);
+}
+
+function verwardeImagesFunctie(e) {
+    var img = e.target;
+    if (img.classList.contains('flip')) {
+        img.classList.remove('flip');
+
+    } else {
+        img.classList.add('flip');
+    }
+    console.log(img);
+}
+
+for (var i = 0; i < bladwijzer.length; i++) {
+    bladwijzer[i].addEventListener('click', bladwijzerFunctie);
+}
+
+for (var j = 0; j < duimgoed.length; j++) {
+    duimgoed[j].addEventListener('click', duimgoedFunctie);
+}
+
+for (var k = 0; k < duimfout.length; k++) {
+    duimfout[k].addEventListener('click', duimfoutFunctie);
+}
+for (var l = 0; l < verwardeImages.length; l++) {
+    verwardeImages[l].addEventListener('click', verwardeImagesFunctie);
 }
